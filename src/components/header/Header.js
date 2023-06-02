724fe224f1fc8ef9a1975624986df2db822fff50
Matheus <matheusweish@gmail.com>
2023-06-02 19:25:35 -0300

    aproaching responsiveness with mobile first

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,15 +1,36 @@
-import { useState } from "react"
+import { useEffect, useRef, useState } from "react"
+import { navData } from "./navdata"
 import "./Header.css"
-import Navbar from "./Navbar"
+import NavBtn from "./NavBtn"
 import Navdrop from "./Navdrop"
 
 function Header() {
+    const [mobileNavOn, setMobileNavOn] = useState(false)
     const [dropState, setDropState] = useState("Hide")
 
+    const navRef = useRef()
+
+    const navBtnElements = navData.map(({title}) => (
+        <NavBtn key={title} title={title} dropState={{dropState, setDropState}} />
+    ))
+
+    function toggleNav() {
+        navRef.current.classList.toggle("header-navbar-toggle")
+        setMobileNavOn(prev => !prev)
+    }
+
     return (
-        <header className="header-container">
-            <div>
-                <Navbar dropState={{dropState, setDropState}} />
+        <header>
+            <div className="main-header-container">
+                <div className="header-icon-container">
+                    <img className="header-navbar-icon" onClick={toggleNav} src={require(`../../assets/images/header-nav-${mobileNavOn ? "close" : "icon"}.png`)} />
+                    <img className="header-search-icon" src={require("../../assets/images/search-glass.png")} alt="magnifying glass icon" />
+                </div>
+                <img className="ps-header-logo" src={require("../../assets/images/playstation-logo.png")} alt="playstation logo" />
+                <button className="header-sign-btn">Sign In</button>
+                <nav ref={navRef} className="header-navbar header-navbar-toggle">
+                    {navBtnElements}
+                </nav>
             </div>
             { dropState !== "Hide" && <Navdrop dropType={dropState}/> }
         </header>
--- src/components/header/NavBtn.js
+++ src/components/header/NavBtn.js
@@ -1,8 +1,11 @@
+import { useState } from "react"
 import "./NavBtn.css"
 
 function NavBtn(props) {
     const {dropState, setDropState} = props.dropState
     const title = props.title
+
+    const [hovered, setHovered] = useState(false)
 
     function handleClick() {
         if (dropState === title) {
@@ -11,10 +14,38 @@ function NavBtn(props) {
         else { setDropState(title) }
     }
 
+    function toggleHovered() {
+        setHovered(prev => !prev)
+    }
+
+    function getBtnIcons(title) {
+        if (dropState === title) {
+            return ({
+                icon: `nav-${title}-filled-blue`,
+                arrow: `filled-arrow`
+            })
+        }
+        else {
+            if (hovered) {
+                return ({
+                    icon: `nav-${title}-outline-blue`,
+                    arrow: `filled-arrow`
+                })
+            }
+            else {
+                return ({
+                    icon: `nav-${title}-outline-gray`,
+                    arrow: `unfilled-arrow`
+                }) 
+            }
+        }
+    }
+
     return (
-        <button onClick={handleClick} className="nav-btn">
+        <button onClick={handleClick} onMouseEnter={toggleHovered} onMouseLeave={toggleHovered} className="nav-btn">
+            <img className="nav-btn-icon" src={require(`../../assets/images/${getBtnIcons(title).icon}.png`)} />
             <span className={`nav-btn-title ${dropState === title ? `nav-btn-title-opened` : ""}`}>{title}</span>
-            <img className={`nav-btn-arrow ${dropState === title ? "nav-btn-arrow-rotate" : ""}`} src={require(`../../assets/images/${dropState === title ? "filled" : "unfilled" }-arrow.png`)} alt="arrow icon pointing down" />
+            <img className={`nav-btn-arrow ${dropState === title ? "nav-btn-arrow-rotate" : ""}`} src={require(`../../assets/images/${getBtnIcons(title).arrow}.png`)} alt="arrow icon pointing down" />
         </button>
     )
 }
