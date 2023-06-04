1d6877eccce04bd2a6c1d1f5129a1f55150321bb
Matheus <matheusweish@gmail.com>
2023-06-04 15:49:12 -0300

    responsive header core logic and styles done

--- src/components/header/Navdrop.js
+++ src/components/header/Navdrop.js
@@ -1,20 +1,14 @@
 import { navData } from "./navdata"
+import NavdropCard from "./NavdropCard"
 import "./Navdrop.css"
 
 function Navdrop(props) {
     const {dropState, setDropState} = props.dropState
-
     const currentData = navData.find(item => item.title === dropState)
-
-    const iconSectionEls = currentData.iconSection.map(item => (
-        <div className="navdrop-icon-el" key={item}>
-            <div>
-                <img className="navdrop-icon-image" src={require(`../../assets/images/${getImageName(item)}.png`)} alt={item} />
-            </div>
-            <span className="navdrop-icon-title">{item}</span>
-        </div>
-    ))
     
+    const cardSectionEls = currentData.iconSection.map(title => (
+        <NavdropCard key={title} title={title}/>
+    ))
     const dotSectionEls = currentData.dotSection.map(item => (
         <div key={item} className="navdrop-dot-el">
             <span className="navdrop-dot-dot"></span>
@@ -22,18 +16,14 @@ function Navdrop(props) {
         </div>
     ))
 
-    function getImageName(elementName) {
-        return elementName.replaceAll(" ", "-")
-    }
-
     return (
             <div className="navdrop-container">
                 <div className="navdrop-title-section">
                     <img onClick={() => setDropState("Hide")} className="navdrop-close-btn" src={require("../../assets/images/drop-menu-icon.png")} alt="close navdrop"/>
                     <p>{dropState}</p>
                 </div>
-                <div className="navdrop-icon-section">
-                    {iconSectionEls}
+                <div className="navdrop-card-section">
+                    {cardSectionEls}
                 </div>
                 <div className="navdrop-dot-section">
                     {dotSectionEls}
